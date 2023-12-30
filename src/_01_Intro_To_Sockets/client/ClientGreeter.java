240bcc1a07994fa5e3db194400bdfd625360c88b
zhaoyd5268 <zhaoyd5268@gmail.com>
2023-12-30 12:57:42 -0800

    Attempt to Finish ChatApp(Level 5, Module 3, Project 3 of 3)

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -5,25 +5,44 @@ import java.io.*;
 
 public class ClientGreeter {
 
-   public static void main(String [] args) {
-	  //1. Create a String for the ip address of the server. 
-	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      
-      //2. Create an integer for the server's port number
-      
-      //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
-    
-    	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
- 
-         //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
-         
-         //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
-         
-         //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
-         
-         //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
-         
-         //9. Close the client's server object
+	public static void main(String[] args) {
+		// 1. Create a String for the ip address of the server.
+		// If you don't know how to find a computer's ip address, ask about ifconfig on
+		// linux/mac and ipconfig on windows.
+		String IPAddress = "localhost";
+		// 2. Create an integer for the server's port number
+		int PortNumber = 9876;
+		// 3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
 
-   }
+		// 4. Create an object of the Socket class. When initializing the object, pass
+		// in the ip address and the port number
+		try {
+			Socket s = new Socket(IPAddress, PortNumber);
+			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+			dos.writeUTF("Poofy");
+			DataInputStream dis = new DataInputStream(s.getInputStream());
+			dis.readUTF();
+			s.close();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// 5. Create a DataOutputStream object. When initializing it, use the Socket
+		// object you created in step 4 to call the getOutputStream() method.
+		
+		// 6. Use the DataOutputStream object to send a message to the server using the
+		// writeUTF(String message) method
+
+		// 7. Create a DataInputStream object. When initializing it, use the Server
+		// object you created in step 4 to call the getInputStream() method.
+
+		// 8. Use the DataInputStream object to print a message from the server using
+		// the readUTF() method.
+
+		// 9. Close the client's server object
+	}
 }
--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,67 @@
 package _02_Chat_Application;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
-public class ChatApp {
+public class ChatApp implements ActionListener {
 
+	String ClientOrServer = JOptionPane.showInputDialog("Would you prefer to be the server or the client?");
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel incoming = new JLabel();
+	JLabel outgoing = new JLabel();
+	JButton button = new JButton();
+	JTextField jtf = new JTextField();
+	public void run() {
+		panel.add(jtf);
+		frame.add(panel);
+		panel.add(outgoing);
+		panel.add(incoming);
+		frame.setVisible(true);
+		button.setText("send");
+		if (ClientOrServer.equalsIgnoreCase("Server")) {
+			Server();
+		}
+	}
+
+	public void Server() {
+		String IPAddress = "localhost";
+		int PortNum = 2963;
+		Boolean b = true;
+		try {
+			Socket s = new Socket(IPAddress, PortNum);
+			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+			button.addActionListener(this);
+		} catch (IOException e) {
+
+		}
+	}
+
+
+	public void actionPerformed(ActionEvent e) {
+		String submit = jtf.getText();
+		outgoing.setText(outgoing + "\n" + submit);
+	}
+
+	public static void main(String[] args) {
+		ChatApp ca = new ChatApp();
+		ca.run();
+	}
+	
 }
